c3bd98423e4e33ab2c25633acde3b2cadbdf2e39
i-van-develop <smartpagedevelop@gmail.com>
2020-03-11 19:53:49 +0200

    add new one to date function

--- src/lib/date.js
+++ src/lib/date.js
@@ -7,3 +7,6 @@ export const createNextMonthDate = (date) => {
 export const createPrevMonthDate = (date) => {
     return new Date(date.getFullYear(), date.getMonth() - 1);
 };
+export const getFirstDayInMonth = (date) => {
+    return (new Date(date.getFullYear(), date.getMonth())).getDay();
+};
